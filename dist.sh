2bca059cd43b8d42161511ef03a283c495f71dc1
Kevin Wojniak <kainjow@users.noreply.github.com>
2021-02-20 15:34:20 -0800

    Check for a universal binary

--- dist.sh
+++ dist.sh
@@ -7,5 +7,12 @@ APP="Semulov"
 rm -rf Dependencies "${BUILD_DIR}"
 xcodebuild -scheme "Semulov Release" "SYMROOT=${BUILD_DIR}"
 pushd "${RELEASE_DIR}"
+
+EXE=${APP}.app/Contents/MacOS/${APP}
+if [[ "$(lipo -info ${EXE})" != *"x86_64 arm64"* ]]; then
+    echo "A universal build must be created. Use Xcode 12.2 or greater."
+    exit 1
+fi
+
 zip -ry "${APP}.zip" "${APP}.app"
 popd
